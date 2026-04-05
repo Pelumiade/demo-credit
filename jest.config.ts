f0a8de7d664a4507bcaf7542db852f7242fa1068
Afolabi <pelumifola@gmail.com>
2026-04-05 18:24:25 +0100

    chore: initialise project with TypeScript and Jest config

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/server.ts', '!src/db/**'],
+  coverageThreshold: {
+    global: { branches: 70, functions: 80, lines: 80, statements: 80 },
+  },
+};
+
+export default config;
